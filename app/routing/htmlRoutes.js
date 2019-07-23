17a8489bb4073b0ba7d71e0102c159bb2b927803
CiSigEp <cisigep@gmail.com>
2019-07-23 13:26:33 -0500

    Add static file serving, homepage, change file path setup

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,10 +2,10 @@ var path = require("path");
 
 module.exports = function(app) {
     app.get("/survey", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "..", "public", "survey.html"));
     });
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "..", "public", "home.html"));
     })
 }
