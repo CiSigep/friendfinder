611b47b65701cfd446838f6f54b6fb6abb76422a
CiSigEp <cisigep@gmail.com>
2019-07-24 15:01:37 -0500

    Add Survey page, AJAX call

--- app/public/assets/js/survey.js
+++ app/public/assets/js/survey.js
@@ -1,3 +1,21 @@
 $(() => {
-    console.log("Running");
+    $("#surveySubmit").on("click", (e) => {
+        var sendData = {
+            name: $("#nameInput").val().trim(),
+            image: $("#imgInput").val().trim(),
+            scores: []
+        };
+
+        $("input[name^=question]:checked").each((i, ele) => {
+            sendData.scores.push($(ele).val());
+        });
+
+        if(sendData.name !== "" && sendData.image !== "" && sendData.scores.length === 10){
+            $.post("/api/friends", sendData).then((data) => {
+                console.log(data);
+            });
+        }
+
+        return false;
+    });
 });
